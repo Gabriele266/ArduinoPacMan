58641243d552fe2200e92652c372e2b32fa6e408
Gabriele <gabri.cabal@gmail.com>
2021-01-20 21:30:42 +0100

    Implementato thread per il caricamento delle librerie da un percorso

---
+++ threads/librariesloader.cpp
@@ -0,0 +1,53 @@
+#include "librariesloader.h"
+
+LibrariesLoader::LibrariesLoader()
+{
+
+}
+
+void LibrariesLoader::setDestination(QTreeWidget *dest){
+    destination = dest;
+}
+
+void LibrariesLoader::setSearchPath(QString path){
+    this->path = path;
+}
+
+void LibrariesLoader::setLibrariesDestination(QList<Library *> *libraries){
+    this->libraries = libraries;
+}
+
+void LibrariesLoader::run(){
+    // Controllo il percorso
+    QDir dir(path);
+
+    // Controllo che esista
+    if(dir.exists()){
+        // Ottengo la lista di tutti i nomi di cartella presenti al suo interno
+        QStringList entries = removeRedundant(dir.entryList());
+
+        // Scorro tutti gli elementi
+        for(int x = 0; x < entries.count(); x++){
+            // Ottengo il percorso completo
+            QDir complete = formatPathForOs(path, QStringList(entries[x]));
+            // Controllo che esista
+            if(complete.exists()){
+                // Potrei aver trovato una libreria
+                Library *lib = new Library();
+                // Imposto il percorso completo
+                lib->setCompletePath(complete.path());
+                // Inizializzo la libreria
+                lib->init();
+                // La aggiungo all' array
+                libraries->append(lib);
+                // Aggiungo un elemento per la libreria nell albero
+                auto *item = new QTreeWidgetItem();
+                item->setText(0, entries[x]);
+                item->setText(1, "Libreria");
+                item->setText(2, complete.path());
+
+                destination->addTopLevelItem(item);
+            }
+        }
+    }
+}
---
+++ threads/librariesloader.h
@@ -0,0 +1,46 @@
+/* Autore: cavallo gabriele
+ * Rappresenta un thread per il caricamento delle librerie da un percorso di partenza
+ * */
+
+#ifndef LIBRARIESLOADER_H
+#define LIBRARIESLOADER_H
+
+#include <QThread>
+#include <QString>
+#include <QStringList>
+#include <QFile>
+#include <QDir>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QList>
+
+#include "../utils/utils.cpp"
+#include "../packages/library.h"
+
+/// Rappresenta un caricatore di librerie su thread separato
+class LibrariesLoader : public QThread
+{
+public:
+    LibrariesLoader();
+
+    /// Esegue il thread
+    void run();
+
+    /// Imposta il percorso in cui cercare le librerie
+    void setSearchPath(QString path);
+
+    /// Imposta l'albero su cui aggiungere le librerie
+    void setDestination(QTreeWidget *dest);
+
+    /// Imposta la lista a cui devono essere aggiunte le librerie trovate
+    void setLibrariesDestination(QList<Library*> *libraries);
+private:
+    // Percorso in cui cercare
+    QString path;
+    // Albero a cui aggiungere i risultati
+    QTreeWidget *destination;
+    // Lista di librerie
+    QList<Library*> *libraries;
+};
+
+#endif // LIBRARIESLOADER_H
