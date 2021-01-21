61409a11a96d51f26ff4eca7bd4a079362c04a24
Gabriele <gabri.cabal@gmail.com>
2021-01-21 15:26:48 +0100

    Implementato Natural per la gestione dei numeri naturali

---
+++ packages/natural.cpp
@@ -0,0 +1,155 @@
+#include "natural.h"
+
+Natural::Natural()
+{
+
+}
+
+unsigned int Natural::abs(int x){
+    if(x < 0){
+        return (-1) * x;
+    }
+    else{
+        return x;
+    }
+}
+
+Natural::Natural(int v){
+    val = Natural::abs(v);
+}
+
+Natural::Natural(QString num){
+    val = Natural::abs(num.toInt());
+}
+
+Natural::Natural(unsigned int *c){
+    val = *c;
+}
+
+void Natural::setValue(int v){
+    // Faccio il valore assoluto
+    val = Natural::abs(v);
+}
+
+void Natural::setValue(bool v){
+    val = v;
+}
+
+void Natural::setValue(Natural v){
+    val = v.getValue();
+}
+
+void Natural::setValue(Natural *v){
+    val = v->getValue();
+}
+
+void Natural::sub(int num){
+    // Prendo il valore assoluto
+    auto absolute = Natural::abs(num);
+    // Controllo se l'operazione ha senso
+    if(absolute < val){
+        // Faccio la differenza
+        val -= absolute;
+    }
+    else{
+        // Underflow
+        val = 0;
+    }
+}
+
+unsigned int Natural::diff(unsigned int n, unsigned int n2){
+    if(n2 < n){
+        return n - n2;
+    }
+    return 0;
+}
+
+unsigned int Natural::getValue(){
+    return val;
+}
+
+Natural::operator unsigned int(){
+    return val;
+}
+
+unsigned int Natural::operator+(Natural left){
+    return left.getValue() + val;
+}
+
+unsigned int Natural::operator+(int v){
+    return val + Natural::abs(v);
+}
+
+unsigned int Natural::operator+=(Natural left){
+    val += left;
+    return val;
+}
+
+unsigned int Natural::operator+=(int v){
+    val += Natural::abs(v);
+    return val;
+}
+
+unsigned int Natural::operator+=(Natural *v){
+    val += *v;
+    return val;
+}
+
+unsigned int Natural::operator-(Natural v){
+    return Natural::diff(val, v);
+}
+
+unsigned int Natural::operator-(int v){
+    return Natural::diff(val, Natural::abs(v));
+}
+
+unsigned int Natural::operator-(Natural *v){
+    return Natural::diff(val, *v);
+}
+
+unsigned int Natural::operator-=(Natural n){
+    sub(Natural::abs(n));
+    return val;
+}
+
+unsigned int Natural::get(int value, NaturalNumberMode mode){
+    switch(mode){
+    case NaturalNumberMode::UseAbsoluteValue:
+        return Natural::abs(value);
+        break;
+    case NaturalNumberMode::ElideUnderZero:
+        if(value < 0){
+            return 0;
+        }
+        else{
+            return value;
+        }
+    }
+}
+
+void Natural::set(int num, NaturalNumberMode mode){
+    val = Natural::get(num, mode);
+}
+
+unsigned int Natural::operator-=(int v){
+    sub(Natural::abs(v));
+    return val;
+}
+
+unsigned int Natural::operator-=(Natural *v){
+    sub(*v);
+    return val;
+}
+
+void Natural::operator=(Natural n){
+    val = n;
+}
+
+void Natural::operator=(Natural *n){
+    val = *n;
+}
+
+// Questo operatore causa una perdita di segno al valore (verrà applicato il valore assoluto)
+void Natural::operator=(int r){
+    val = Natural::abs(r);
+}
---
+++ packages/natural.h
@@ -0,0 +1,88 @@
+/* Autore: cavallo gabriele
+ * Rappresenta un numero naturale e fornisce semplificazioni per il suo uso
+ * */
+
+#ifndef NATURAL_H
+#define NATURAL_H
+
+#include <QString>
+
+/// Indica le operazioni da fare quando si vuole passare da un numero intero a un naturale
+enum NaturalNumberMode{
+    // Indica di usare il valroe assoluto del numero (-1 --> 1)
+    UseAbsoluteValue,
+    // Indica di convertire tutti i valori sotto lo zero a 0
+    // -1 --> 0
+    ElideUnderZero
+};
+
+/// Rappresenta un numero naturale
+class Natural
+{
+public:
+    Natural();
+    Natural(int v);
+    Natural(unsigned int *v);
+    Natural(QString val);
+
+    /// Imposta il valore
+    void setValue(int val);
+    void setValue(bool v);
+    void setValue(Natural v);
+    void setValue(Natural *val);
+
+    /// Restituisce il valore per l'uso con le operazioni matematiche
+    operator unsigned int();
+
+    unsigned int getValue();
+
+    /// Aggiunge un numero prendendo solo il valore assoluto
+    void add(int val);
+    void add(Natural val);
+    void add(Natural *val);
+
+    /// Toglie il valore assoluto di num al valore corrente
+    void sub(int num);
+
+    /// Imposta il valore del numero applicando una politica precisa
+    void set(int num, NaturalNumberMode mode);
+
+    /// Permette di ottenere il valore senza segno applicando una politica precisa
+    static unsigned int get(int value, NaturalNumberMode mode);
+
+    /// Effettua la differenza tra due numeri senza segno
+    static unsigned int diff(unsigned int n, unsigned int n2);
+
+    unsigned int operator+(Natural left);
+    unsigned int operator+(int v);
+
+    unsigned int operator+=(Natural left);
+    unsigned int operator+=(int v);
+    unsigned int operator+=(Natural *val);
+
+    unsigned int operator-(Natural val);
+    unsigned int operator-(int val);
+    unsigned int operator-(Natural *val);
+
+    unsigned int operator-=(Natural n);
+    unsigned int operator-=(int val);
+    unsigned int operator-=(Natural *v);
+
+    void operator=(Natural n);
+    void operator=(Natural *n);
+    // Questo operatore causa una perdita di segno al valore (verrà applicato il valore assoluto)
+    void operator=(int r);
+
+    bool operator==(Natural n);
+    /// Confronta il valore corrente con il valore assoluto del parametro
+    bool operator==(int val);
+    bool operator==(Natural *val);
+
+    /// Calcola il valore assoluto del numero intero con segno e lo restituisce
+    static unsigned int abs(int x);
+private:
+    // Valore del numero
+    unsigned int val;
+};
+
+#endif // NATURAL_H
