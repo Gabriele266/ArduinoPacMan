4c163deea2e751fdda11a1c4d0f7334c8db5be78
Gabriele <gabri.cabal@gmail.com>
2021-01-27 17:14:22 +0100

    Implementata classe per gestione di un sorgente e delle sue info

--- packages/source.cpp
+++ packages/source.cpp
@@ -2,5 +2,51 @@
 
 Source::Source()
 {
+    file_path = "";
+    file_suffix = "";
+    source_name = "";
+    file = "";
+    type = Unknown;
+    dependencies.clear();
+}
 
+Source::Source(QString f){
+    if(file != ""){
+        QFileInfo info(f);
+        file_path = info.filePath();
+        source_name = info.fileName();
+        file_suffix = info.suffix();
+        file = f;
+
+        if(file_suffix == "h"){
+            type = HeaderFile;
+        }
+        else if(file_suffix == "cpp" || file_suffix == "c"){
+            type = ImplementationFile;
+        }
+        else if(file_suffix == "ino"){
+            type = ArduinoSketch;
+        }
+        else{
+            qInfo() << "Trovato file non riconosciuto: " << file << endl;
+            type = Unknown;
+        }
+    }
+}
+
+Source::Source(QFileInfo info){
+    file_path = info.filePath();
+    file_suffix = info.suffix();
+    source_name = info.fileName();
+    file = info.absoluteFilePath();
+}
+
+bool Source::fileIsSource(QString file){
+    // Informazioni sul file
+    QFileInfo inf(file);
+    // Estensione
+    QString suff = inf.suffix();
+
+    // Controllo se è presente nella lista
+    return isAnyOfList(suff, sources_extensions);
 }
--- packages/source.h
+++ packages/source.h
@@ -1,3 +1,7 @@
+/*  Autore: cavallo gabriele
+ *  Fornisce metodi di gestione dei file sorgenti
+ * */
+
 #ifndef SOURCE_H
 #define SOURCE_H
 
@@ -6,26 +10,73 @@
 
 #include "dependency.h"
 #include "natural.h"
+#include "../utils/macros.h"
+#include "../utils/utils.cpp"
+#include "../utils/costants.h"
 
+/// Rappresenta un tipo di sorgente
 enum SourceType{
+    /// File di implementazione (.cpp)
     ImplementationFile,
+    /// File header (.h)
     HeaderFile,
-    ArduinoSketch
+    /// Scketch di arduino (.ino)
+    ArduinoSketch,
+    /// Estensione sconosciuta
+    Unknown
 };
 
-
+/// Fornisce metodi di gestione dei file sorgenti
 class Source
 {
 public:
     Source();
+    Source(QString file);
+    Source(QFileInfo info);
 
+    /// Restituisce il percorso relativo del file (senza nome e estensione)
+    GETTER(QString, file_path, FilePath)
+
+    /// Restituisce l'estensione del file
+    GETTER(QString, file_suffix, FileSuffix)
+
+    /// Restituisce il tipo di sorgente
+    GETTER(SourceType, type, SourceType)
+
+    /// Aggiunge una dipendenza del file
+    void appendDependency(Dependency *dep);
+
+    /// Restituisce una lista con tutte le dipendenze del file
+    QList<Dependency*>* getDependenciesList();
+
+    /// Imposta il file del sorgente
+    GETTER(QString, file, CompleteFile)
+
+    /// Imposta il file
+    void setCompleteFile(QString file);
+
+    /// Determina se questo file è un sorgente di codice o qualcos' altro
+    bool isSource();
+
+    /// Determina se questo file è un sorgente di codice o qualcos' altro
+    static bool fileIsSource(QString file);
 private:
     // Percorso del file
-    QString filePath;
+    QString file_path;
 
-    // Lista di dipendenze
-//    QList<Dependency*> dependencies;
+    // Estensione del file
+    QString file_suffix;
 
+    // Nome del sorgente
+    QString source_name;
+
+    QString file;
+
+    // Tipo di sorgente
+    SourceType type;
+
+    // Lista di dipendenze del sorgente
+    QList<Dependency*> dependencies;
 };
 
 #endif // SOURCE_H
