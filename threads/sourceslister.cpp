3d7d33d1222e6cce650abca1ddd28f6d4063792e
Gabriele <gabri.cabal@gmail.com>
2021-01-28 13:22:07 +0100

    Corretti bug implementata eccezione commenti

--- threads/sourceslister.cpp
+++ threads/sourceslister.cpp
@@ -20,6 +20,7 @@ void SourcesLister::appendDependencyToWidget(Source *src, Dependency *dep, Natur
             item->setText(2, row.toString());
             item->setText(3, "No");
             item->setIcon(0, QIcon(":/icons/browsing/dependencies.png"));
+            item->setIcon(3, QIcon(":/icons/browsing/browsing/wrong.png"));
             if(parent != nullptr){
                 parent->addChild(item);
             }
@@ -33,6 +34,8 @@ void SourcesLister::appendDependencyToWidget(Source *src, Dependency *dep, Natur
 void SourcesLister::run(){
     // Creo l'oggetto file
     QFile file(startFile->getCompleteFile());
+    // Indica lo stato del commento
+    bool comment_state = false;
 
     if(file.open(QIODevice::ReadOnly)){
         // Creo uno stream di testo
@@ -45,27 +48,47 @@ void SourcesLister::run(){
             // Leggo la riga corrente
             QString curLine = str.readLine();
             // Controllo se si tratta di una direttiva al preprocessore per una inclusione di libreria
-            if(curLine.startsWith("#include")){
+            if(curLine.startsWith("//")){
+                comment_state = true;
+            }
+            else if(curLine.contains("/*")){
+                if(!curLine.contains("*/")){
+                    comment_state = true;
+                }
+                else{
+                    comment_state = false;
+                }
+            }
+            else if(curLine.contains("*/")){
+                comment_state = false;
+            }
+            else if(curLine.startsWith("#include") && !comment_state){
                 // Prendo il contenuto dell' header richiesto
-                QString head = curLine.mid(curLine.indexOf('<') + 1, curLine.indexOf('>') - 1);
-                // Rimuovo gli spazi
-                head = head.remove('>');
-                qInfo() << "Header richiesto: " << head;
-                // Prendo il nome della libreria
-                QStringList div = head.split('.');
-                if(div.count() == 2){
-                    QString lib_name = div[0];
+                // Controllo se si tratta di una inclusione globale o di un file locale
+                if(curLine.contains('<')){
+                    QString head = curLine.mid(curLine.indexOf('<') + 1, curLine.indexOf('>') - 1);
+                    // Rimuovo gli spazi
+                    head = head.remove('>');
+                    qInfo() << "Header richiesto: " << head;
+                    // Prendo il nome della libreria
+                    QStringList div = head.split('.');
+                    if(div.count() == 2){
+                        QString lib_name = div[0];
 
-                    // Formatto una dipendenza
-                    Dependency *d = new Dependency();
-                    d->setLibraryName(lib_name);
-                    d->setHeaderName(head);
-                    // Aggiungo la dipendenza alla lista
-                    resultsList->append(d);
-                    // Aggiungo la dipendenza al sorgente
-                    startFile->appendDependency(d);
-                    // Appendo al controllo
-                    appendDependencyToWidget(startFile, d, current_row);
+                        // Formatto una dipendenza
+                        Dependency *d = new Dependency();
+                        d->setLibraryName(lib_name);
+                        d->setHeaderName(head);
+                        // Aggiungo la dipendenza alla lista
+                        resultsList->append(d);
+                        // Aggiungo la dipendenza al sorgente
+                        startFile->appendDependency(d);
+                        // Appendo al controllo
+                        appendDependencyToWidget(startFile, d, current_row);
+                    }
+                }
+                else{
+                    qInfo() << "Inclusione locale non ancora supportata. " << endl;
                 }
             }
             current_row ++;
